57aad136f8f2c8aebc74df0880b694792c07328e
AseelBu <57968652+AseelBu@users.noreply.github.com>
2020-07-04 17:39:50 +0300

    newProduct button works

--- x.php
+++ x.php
@@ -1,4 +1,20 @@
 <?php
+require("db.php");
 header('Content-Type: application/json');
 
-echo json_encode("hallo");
+$productName="Avocado";
+
+$sql = "SELECT * FROM `product` WHERE `name`like '$productName'";
+
+$result = $conn->query($sql);
+
+
+if ($result->num_rows>0){
+//product exists
+
+    $product = $result->fetch_assoc();
+    $productId = $product['id'];
+    echo json_encode(TRUE);
+}
+
+$conn->close();
